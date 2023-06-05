80eb1474b51cadbab1204c0e1ccfe3db9b84eb59
idrissa-gif <idrissa@iut-dhaka.edu>
2023-06-05 16:16:05 +0600

    Updates

---
+++ precache-manifest.12f9df1896baa59847b3bd84d5ef4e4f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9016018897c7e58f7d911d0ac0310dad",
+    "url": "/Real_ImageToCartoon_Image_GAN/index.html"
+  },
+  {
+    "revision": "abe22b2f83e8d6f32bb1",
+    "url": "/Real_ImageToCartoon_Image_GAN/static/css/2.741e3868.chunk.css"
+  },
+  {
+    "revision": "a04921a181d15eb5653c",
+    "url": "/Real_ImageToCartoon_Image_GAN/static/css/main.461c7a03.chunk.css"
+  },
+  {
+    "revision": "abe22b2f83e8d6f32bb1",
+    "url": "/Real_ImageToCartoon_Image_GAN/static/js/2.4ac0a219.chunk.js"
+  },
+  {
+    "revision": "5e2252a167034352fa7bed0a873eaa75",
+    "url": "/Real_ImageToCartoon_Image_GAN/static/js/2.4ac0a219.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a04921a181d15eb5653c",
+    "url": "/Real_ImageToCartoon_Image_GAN/static/js/main.588943d0.chunk.js"
+  },
+  {
+    "revision": "ab2640607409e11b9065",
+    "url": "/Real_ImageToCartoon_Image_GAN/static/js/runtime-main.e9b895f3.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.1229ccb53106cb99ab06e9d81657ede7.js"
+  "/Real_ImageToCartoon_Image_GAN/precache-manifest.12f9df1896baa59847b3bd84d5ef4e4f.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Real_ImageToCartoon_Image_GAN/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
